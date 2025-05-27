dd8721c8433e87cfb34de26449e614b143983eed
ygor lima <115748634+ygorlvieira@users.noreply.github.com>
2025-05-27 15:46:58 -0300

    Corrigido e adicionado algumas secoes e elementos

--- main.js
+++ main.js
@@ -68,3 +68,67 @@ var swiper = new Swiper(".blogs-slider", {
         },
     },
 });
+
+// Accordion functionality
+    const accordions = document.querySelectorAll('.accordion-title');
+
+    accordions.forEach(item => {
+        item.addEventListener('click', () => {
+            const parent = item.parentElement;
+            parent.classList.toggle('active');
+        });
+    });
+
+
+    //Modal functionality
+
+ 
+    const modal = document.getElementById("modal");
+    const modalImg = document.getElementById("modal-img");
+    const slides = document.querySelectorAll(".gallery .swiper-slide img");
+    const closeBtn = document.querySelector(".modal .close");
+    const prevBtn = document.querySelector(".modal .prev");
+    const nextBtn = document.querySelector(".modal .next");
+
+    let currentIndex = 0;
+
+    function showModal(index) {
+        modal.style.display = "block";
+        modalImg.src = slides[index].src;
+        currentIndex = index;
+    }
+
+    slides.forEach((img, index) => {
+        img.addEventListener("click", () => {
+            showModal(index);
+        });
+    });
+
+    closeBtn.addEventListener("click", () => {
+        modal.style.display = "none";
+    });
+
+    modal.addEventListener("click", (e) => {
+        if (e.target === modal) {
+            modal.style.display = "none";
+        }
+    });
+
+    prevBtn.addEventListener("click", (e) => {
+        e.stopPropagation();
+        currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+        showModal(currentIndex);
+    });
+
+    nextBtn.addEventListener("click", (e) => {
+        e.stopPropagation();
+        currentIndex = (currentIndex + 1) % slides.length;
+        showModal(currentIndex);
+    });
+
+// impedir rolamento da página ao abrir o modal
+document.addEventListener("wheel", function(event) {
+    if (modal.style.display === "block") {
+        event.preventDefault();
+    }
+}, { passive: false });
